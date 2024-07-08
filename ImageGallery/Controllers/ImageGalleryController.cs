0455f8ac8273e73ef4c232f25e1b8af129c72646
harsh <harsh@DESKTOP-1GI35PE>
2024-07-08 12:29:26 +0530

    Add project files.

---
+++ ImageGallery/Controllers/ImageGalleryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ImageGallery.Controllers
+{
+    public class ImageGalleryController : Controller
+    {
+        private readonly string rootPath = Path.Combine
+            (Directory.GetCurrentDirectory(),"wwwroot//uploads");
+        public IActionResult Index()
+        {
+            //create directory
+            bool dirExists = Directory.Exists (rootPath);
+
+            if (!dirExists)
+            {
+                Directory.CreateDirectory(rootPath);
+            }
+			List<string> imageNames = Directory.GetFiles(rootPath).Select(Path.GetFileName).ToList();
+            return View(imageNames);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            if (file != null)
+            {
+                //get file name
+                var path = Path.Combine(rootPath, Guid.NewGuid() +
+                    Path.GetExtension(file.FileName));
+
+                //upload file directory
+                using (var fs = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(fs);
+                }
+                return RedirectToAction(nameof(Index));
+                //return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
